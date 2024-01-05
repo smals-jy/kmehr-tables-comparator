d4de5d39d9e47ec11f08997f517ae4d9381f3587
Jacques Yakoub <jacques.yakoub@smals.be>
2024-01-05 15:52:26 +0100

    ci: fix script

--- labels-fetcher.sh
+++ labels-fetcher.sh
@@ -2,7 +2,7 @@
 
 # Step 1 : Extract links and references names from the website
 page_content=$(curl -s https://www.ehealth.fgov.be/standards/kmehr/en/tables)
-link=$(echo "$page_content" | grep -oP '<a[^>]*\shref="([^"]*)"[^>]*class="btn btn-primary icon-kmehr"[^>]*>\s*xml\s*</a>')
+links=$(echo "$page_content" | grep -oP '<a[^>]*\shref="([^"]*)"[^>]*class="btn btn-primary icon-kmehr"[^>]*>\s*xml\s*</a>')
 
 # Count the number of links found
 num_links=$(echo "$links" | wc -l)
@@ -15,7 +15,7 @@ echo "Created temp directory."
 echo "$links" | while read -r line; do
 
     # Prepare variables
-    table=$(echo "$link" | sed -n 's/.*name=\([^&]*\).*/\1/p' | tr '[:lower:]' '[:upper:]')
+    table=$(echo "$line" | sed -n 's/.*[?&]name=\([^&]*\).*/\1/p' | tr '[:lower:]' '[:upper:]')
     full_href=$(echo "$line" | sed -n 's/.*href="\([^"]*\).*/\1/p')
 
     # Step 2: Download the files
