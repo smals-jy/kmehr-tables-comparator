9f0f4e584dcf4cce8c444da904237cade4a0b9e5
Jacques Yakoub <jacques.yakoub@gmail.com>
2024-01-13 18:43:04 +0100

    ci: Update generate-labels-for-cd-tables.ts

--- generate-labels-for-cd-tables.ts
+++ generate-labels-for-cd-tables.ts
@@ -1,6 +1,6 @@
 import extractLabelsFromXML from "./src/plugins/kmehr/extract-labels";
 import { resolve as pathResolver } from "path";
-import { readdir, writeFile, access, constants as FS_constants } from "fs/promises"
+import { readdir, writeFile } from "fs/promises"
 
 const KMEHR_FOLDER = pathResolver("static/tables");
 
@@ -16,14 +16,8 @@ async function main() {
         const path_to_cd = pathResolver(KMEHR_FOLDER, kmehr_table, `labels.xml`);
         const output = pathResolver(KMEHR_FOLDER, kmehr_table, `labels.json`);
         try {
-            try {
-                await access(output, FS_constants.F_OK);
-                console.log("\t Result file does exist - Skipping ...");
-            } catch (err) {
-                console.log(`\t Result file doesn't exist - Creating ...`);
-                const tables = await extractLabelsFromXML(path_to_cd);
-                await writeFile(output, JSON.stringify(tables, null, "\t"));
-            }
+            const tables = await extractLabelsFromXML(path_to_cd);
+            await writeFile(output, JSON.stringify(tables, null, "\t"));
         } catch (err) {
             console.log(`\t Something went wrong`);
             console.error(err);
